74555a99e6b139be6bc75479b08d9250799d0183
Terence Nathan <terenceloorthanathan43@gmail.com>
2023-06-17 14:00:24 +0800

    first commit

---
+++ pages/footeruser.php
@@ -0,0 +1,15 @@
+</div>
+			
+            
+            <div class="footer-wrap pd-20 mb-20 card-box">
+				Sentiment Analysis by <a href="https://github.com/drshahizan/special-topic-data-engineering/tree/main/project/submission/DataSphere" target="_blank">DataShpere</a>
+			</div>
+		</div>
+	</div>
+	<!-- js -->
+	<script src="vendors/scripts/core.js"></script>
+	<script src="vendors/scripts/script.min.js"></script>
+	<script src="vendors/scripts/process.js"></script>
+	<script src="vendors/scripts/layout-settings.js"></script>
+</body>
+</html>
